348f0e0c9db3c95f78def3eb12e99b33815c85c0
Mitsutaka Okazaki <okaxaki@users.noreply.github.com>
2023-08-20 04:39:04 +0900

    Disable debug server configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
-import fs from 'fs';
+// import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,11 +15,11 @@ export default defineConfig({
       },
     }),
   ],
-  server: {
-    https: {
-      key: fs.readFileSync('./cert/localhost+2-key.pem'),
-      cert: fs.readFileSync('./cert/localhost+2.pem'),
-    }
-  },
+  // server: {
+  //   https: {
+  //     key: fs.readFileSync('./cert/localhost+2-key.pem'),
+  //     cert: fs.readFileSync('./cert/localhost+2.pem'),
+  //   }
+  // },
   // resolve: { preserveSymlinks: true },
 });
